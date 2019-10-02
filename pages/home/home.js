51cd40aac1fe955548895d9ca598f6e0ed19635a
liu_ziding@163.com <liu_ziding@163.com>
2019-10-02 15:09:25 +0800

    弹窗和分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,69 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中...',
+      duration: 3000,
+      icon: 'loading',
+      mask: true,
+      success: function() {
+        console.log('展示弹窗成功')
+      },
+      fail: function() {
+        console.log('展示弹窗失败')
+      },
+      complete: function() {
+        console.log('完成函数的调用')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal() {
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容，哈哈哈',
+      // showCancel: false,
+      // cancelText: '退出',
+      success: function(res) {
+        console.log(res)
+        if(res.cancel) {
+          console.log('用户点击了取消')
+        } else {
+          console.log('用户点了确定按钮')
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载...',
+      mask: true
+    })
+    setTimeout(() => {
+      // 必须手动hideLoading 才会让 loading 消失
+      wx.hideLoading()
+    }, 5000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowactionSheet() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'red',
+      success: function(res) {
+        switch(res.tapIndex) {
+          case 0: console.log('相册');
+            break;
+          case 1: console.log('拍照');
+            break;
+          default:
+            break;
+        }
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(options) {
+    return {
+      title: '你好，刘自定',
+      path: '/pages/about/about',
+      imageUrl: '../../assets/liuziding.jpg'
+    }
   }
 })
