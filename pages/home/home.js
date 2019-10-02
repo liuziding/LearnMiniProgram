a0597ffa7883526a44dda20881841f792d6421f7
liu_ziding@163.com <liu_ziding@163.com>
2019-10-02 14:08:16 +0800

    网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,58 @@
 // pages/home/home.js
+import request from '../../service/network.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
+  
+  onLoad: function(options) {
+    // 1. 原生的方式发送网络请求
+    // this.get_data_origin()
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    //2. 使用封装的 request 发送网络请求
+    // Promise 最大的好处就是防止出现回调地狱
+    request({
+      url: 'http://123.207.32.32:8000/api/v1/recommend'
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  get_data_origin() {
+    // 发送最简单的 get 请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/api/v1/recommend',
+    //   success: function(res) {
+    //     console.log(res)
+    //   }
+    // })
+    // 2.get 请求，但是携带参数
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/api/v1/home/data',
+    //   method: 'post',
+    //   data: {
+    //     type: 'sell',
+    //     page: 1
+    //   },
+    //   success: function (res) {
+    //     console.log(res)
+    //   }
+    // })
+    // 3.post 请求，并且携带参数
+    wx.request({
+      url: 'http://httpbin.org/post',
+      method: 'post',
+      data: {
+        type: 'LiuZiDing',
+        age: 18
+      },
+      success: function (res) {
+        console.log(res)
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
